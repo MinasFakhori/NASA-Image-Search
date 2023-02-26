7679eca8ecb6d5c15a8f4d7a670d0fb175c61c98
Minas <minasfakhori1@outlook.com>
2023-02-26 21:10:01 +0000

    Commented all the code

--- js/main.js
+++ js/main.js
@@ -18,13 +18,14 @@ window.addEventListener("load", () => {
   const focusDesc = document.querySelector("#focus_desc");
   const clearAll = document.querySelector("#clear_all");
 
-  let imgInFocus = false;
-  let scrollPosition;
-  let isHomePage = true;
+  let scrollPosition; // Variable to store scroll position.
+  let imgInFocus = false; // Boolean to check if image is in focus, if it is then don't allow onClick on other images.
+  let isHomePage = true; // Boolean to check if user is on home page or not, so the clear button position can be changed accordingly.
 
   searchField.addEventListener("input", (e) => {
     let className = isHomePage ? "clear_class_search" : "clear_class_searched";
 
+    // If the search field is not empty, show the clear button.
     if (e.target.value.length > 0) {
       clearAll.classList.add(className);
       clearAll.classList.remove("clear_class_hidden");
@@ -46,21 +47,25 @@ window.addEventListener("load", () => {
     e.preventDefault();
     const searchText = document.querySelector("#search_input").value;
     if (searchText.trim().length < 1) {
-      defaultPage();
+      defaultPage(); // If the search field is empty, go to the home page.
     } else {
       const data = encodeURIComponent(searchText);
       getQuery(data, false);
     }
   });
 
+  /*
+  isHistory is a boolean to check if the user is going back to a previous page or not, if they are I don't want to add that to the history state.
+  isHistory stops from the history state being in a infinite loop.
+   */
   const getQuery = (data, isHistory) => {
     loadingPage();
     removeItems();
     const text = decodeURIComponent(data);
 
-    const xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest(); // Create a new XMLHttpRequest object.
     xhr.addEventListener("load", () => {
-      if (xhr.status === 200) {
+      if (xhr.status === 200) { // 200 means the request is successful.
         resultPage(data, text, isHistory);
         const response = JSON.parse(xhr.responseText);
         const items = response.collection.items;
@@ -74,7 +79,7 @@ window.addEventListener("load", () => {
       }
     });
 
-    xhr.open("GET", `https://images-api.nasa.gov/search?q=${data}`, true);
+    xhr.open("GET", `https://images-api.nasa.gov/search?q=${data}`, true); // Open a new connection, using the GET request on the URL endpoint.
     xhr.send();
   };
 
@@ -85,7 +90,7 @@ window.addEventListener("load", () => {
         const imgTitle = item.data[0].title;
         const imgDesc = item.data[0].description;
 
-        createImageElement(imgSrc, imgTitle, imgDesc);
+        createImageElement(imgSrc, imgTitle, imgDesc); 
       }
     }
   };
@@ -113,17 +118,17 @@ window.addEventListener("load", () => {
     imgInFocus = false;
     focus.style.display = "none";
 
-    window.scrollTo(0, scrollPosition);
+    window.scrollTo(0, scrollPosition); // Scroll back to the previous position.
   });
 
-  const removeItems = () => {
+  const removeItems = () => { // Remove all the images from the output container.
     while (outputContainer.firstChild) {
       outputContainer.removeChild(outputContainer.firstChild);
     }
   };
 
   const defaultPage = () => {
-    document.title = "NASA Image Search";
+    document.title = "NASA Image Search"; // Change the title of the page.
     isHomePage = true;
 
     title.style.display = "flex";
@@ -181,8 +186,8 @@ window.addEventListener("load", () => {
 
     if (!isHistory) {
       history.pushState(
-        { state: `search:${data}` },
-        text,
+        { state: `search:${data}` }, // Make the state an object with a prefix to know what type of state it is and the data. 
+        text, // The title of the page, most browsers don't use this yet but it's for future proofing.
         `search+${data}.html`
       );
     }
@@ -219,7 +224,6 @@ window.addEventListener("load", () => {
     body.classList.add("searched_body");
     form.classList.add("search_class_hidden");
 
-
     errorBtn.addEventListener("click", () => {
       defaultPage();
       searchField.value = "";
@@ -227,16 +231,18 @@ window.addEventListener("load", () => {
   };
 
   window.addEventListener("popstate", (e) => {
-    imgInFocus = false;
+    imgInFocus = false; // Reset the image in focus variable.
     if (e.state.state === "index") {
       defaultPage();
       searchField.value = "";
-    } else if (e.state.state.startsWith("search")) {
-      getQuery(e.state.state.split(":")[1], true);
+    } else if (e.state.state.startsWith("search")) { 
+      getQuery(e.state.state.split(":")[1], true); 
+      // Using split I can get all the data after the colon. 1 to say second part of the string.
       searchField.value = decodeURIComponent(e.state.state.split(":")[1]);
       // searchField.setAttribute('value', e.state.state.split(":")[1]);
     }
   });
 
+  // This replaces the default state with a custom state so I can use it in the popstate.
   history.replaceState({ state: "index" }, "default", "index.html");
 });
